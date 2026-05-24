38c9ae6b1114e91355e77445c78d3c242f6b2f46
youknowhim <pallavrai39@gmail.com>
2026-05-24 19:20:05 +0530

    admin chnages

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -15,6 +15,6 @@ router.post('/create-user', auth, role('Admin'), createUser);
 router.get('/pending', auth, role('Admin'), pending);
 router.patch('/approve/:user_code', auth, role('Admin'), approve);
 router.patch('/reject/:user_code', auth, role('Admin'), reject);
-router.patch('/change-password',role('admin'),changePassword);
+router.patch('/change-password',role('Admin'),changePassword);
 router.patch('/users/:user_code/role', auth, role('Admin'), changeUserRole);
 module.exports = router;
